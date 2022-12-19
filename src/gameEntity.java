0ddb4b5e08b5cf591f9dde5cbe9cc6086d1f6823
aduwa6 <adirouse8@gmail.com>
2022-12-19 11:18:43 -0800

    Initial commit

---
+++ src/gameEntity.java
@@ -0,0 +1,23 @@
+public class gameEntity {
+    private int health;
+    private int damage;
+    private int protection;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+
+}
